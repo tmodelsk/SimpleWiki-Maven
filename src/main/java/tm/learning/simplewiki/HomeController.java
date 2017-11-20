5f2c9fec0451474bffd9b0582aa1e9d23b9b3e2f
Tomasz Modelski <OrdoRerumDev@gmail.com>
2017-11-21 00:26:19 +0100

    Wiki controller mapping draft

--- src/main/java/tm/learning/simplewiki/HomeController.java
+++ src/main/java/tm/learning/simplewiki/HomeController.java
@@ -8,6 +8,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -19,11 +20,25 @@ public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
+	public String homePage(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		String pageName = "home";
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		model.addAttribute("message", "Some msg from Home Controller, Home=" + pageName );
+		
+		return "home";
+	}
+	
+	@RequestMapping(value = "/{pageName}", method = RequestMethod.GET)
+	public String viewPage(Locale locale, Model model, @PathVariable("pageName") String pageName) {
 		logger.info("Welcome home! The client locale is {}.", locale);
 		
 		Date date = new Date();
@@ -32,7 +47,22 @@ public class HomeController {
 		String formattedDate = dateFormat.format(date);
 		
 		model.addAttribute("serverTime", formattedDate );
-		model.addAttribute("message", "Some msg from Home Controller" );
+		model.addAttribute("message", "Some msg from Home Controller, page=" + pageName );
+		
+		return "home";
+	}
+	
+	@RequestMapping(value = "/{pageName}/edit", method = RequestMethod.GET)
+	public String editPage(Locale locale, Model model, @PathVariable("pageName") String pageName) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		model.addAttribute("message", "Edit "+pageName );
 		
 		return "home";
 	}
