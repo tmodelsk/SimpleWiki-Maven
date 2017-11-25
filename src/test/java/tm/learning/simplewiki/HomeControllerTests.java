a79e5db37dec8451d9f0ee7b2c27445aa764b2e4
Tomasz Modelski <OrdoRerumDev@gmail.com>
2017-11-25 17:21:51 +0100

    First Spring MBV unit mocked tests

---
+++ src/test/java/tm/learning/simplewiki/HomeControllerTests.java
@@ -0,0 +1,50 @@
+package tm.learning.simplewiki;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.server.MockMvc;
+import org.springframework.test.web.server.ResultActions;
+import org.springframework.test.web.server.request.MockMvcRequestBuilders;
+import org.springframework.test.web.server.setup.MockMvcBuilders;
+
+import lombok.Getter;
+import lombok.val;
+
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.server.request.MockMvcRequestBuilders.*; 
+import static org.springframework.test.web.server.result.MockMvcResultMatchers.*;
+
+public class HomeControllerTests {
+	
+	@InjectMocks 
+	HomeController controller;
+	
+	MockMvc mockMvc;
+	
+	@Before
+	public void setUp() {
+		MockitoAnnotations.initMocks(this); 
+		
+		mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+		
+	}
+	
+	@Getter
+	String ccccc;
+	
+	@Test
+	public void someTest() throws Exception {
+		//Model modelMap = new Model();
+		//String viewName = controller.homePage(null, model);
+		
+		//val x = "sdfsd";
+		
+		
+		ResultActions perform = mockMvc.perform(get("/", 1l));
+		
+	}
+	
+	
+}
